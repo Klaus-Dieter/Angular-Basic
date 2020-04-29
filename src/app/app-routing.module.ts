bc696bd239f97f013281e3ceeec705d4977b4da8
Klaus-Dieter Wahl <klaus.d.wahl@gmail.com>
2020-04-29 12:35:37 +0200

    Package Material included

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MaterialModule }from './material.module';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    MaterialModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
